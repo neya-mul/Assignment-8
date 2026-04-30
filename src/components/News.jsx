f70d904d718a4098a8bdeb838afe87e8dfe0efec
neya-mul <neyamulislam946@gmail.com>
2026-04-30 10:32:17 +0600

    marquee added

---
+++ src/components/News.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Marquee from 'react-fast-marquee'
+
+
+export default function News() {
+    const marqueeItems = [
+        {
+            id: 1,
+            type: "new_arrival",
+            text: "The Silent Forest"
+        }
+
+    ]
+
+  
+    return (
+        <Marquee className='flex gap-4'>
+            <span className='mx-9'> New arraival :{marqueeItems.map(news => <span className='text-xl font-bold' key={news.id}>{news.text} </span>)} </span>
+            <span className='mx-9'> Special discount on <span className='text-xl font-bold'>Membership</span> </span>
+        </Marquee>
+    )
+}
