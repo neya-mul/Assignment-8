866964f43e9246d471d0a08d813acbc396b63efb
neya-mul <neyamulislam946@gmail.com>
2026-05-02 19:32:42 +0600

    filtering sidebar added

--- src/app/all-books/page.jsx
+++ src/app/all-books/page.jsx
@@ -8,25 +8,54 @@ export default function AllBooks() {
   // const books = await getBooks()
   const [name, setName] = useState('')
   // console.log(books);
+  const [category, setCategory] = useState('')
 
-  useEffect(()=>{
-    const fetchBooks = async () =>{
+  useEffect(() => {
+    const fetchBooks = async () => {
       const data = await getBooks()
       setBooks(data)
     }
     fetchBooks()
-  },[])
+  }, [])
 
   const search = (e) => {
     setName(e.target.value)
 
   }
-  const filterBooks = books.filter(book=>
-    book.title.toLowerCase().includes(name.toLowerCase())
+  const filterBooks = books.filter(book => {
+    const matchName = book.title.toLowerCase().includes(name.toLowerCase())
+    const matchCategory = category ? book.category.toLowerCase() === category.toLowerCase() : true
+    return matchCategory && matchName
+  }
   )
+
+  const handleCategory = (c) => {
+    setCategory(c)
+    document.getElementById('my-drawer-1').checked = false;
+  }
 
   return (
     <div className=' mt-30'>
+      <div className="drawer">
+        <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {/* Page content here */}
+          <label htmlFor="my-drawer-1" className="btn drawer-button">Filter by Category</label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay"></label>
+          <ul className="menu bg-base-200 min-h-fit mt-[20%] w-80 p-4">
+            {/* Sidebar content here */}
+            <li onClick={() => handleCategory('')}><a>All</a></li>
+            <li onClick={() => handleCategory('story')}><a>Story</a></li>
+            <li onClick={() => handleCategory('tech')}><a>Tech</a></li>
+            <li onClick={() => handleCategory('science')}><a>Science</a></li>
+          </ul>
+        </div>
+      </div>
+
+
+
       <div className='text-center'>
         <h1 className='text-6xl'>All Books</h1>
         <p className='max-w-[800px] mx-auto my-3'>"Every book you read is a life you didn't have to live to learn from. Open one, and you open a door to a thousand worlds, a thousand minds, a thousand versions of who you could become."</p>
@@ -45,7 +74,7 @@ export default function AllBooks() {
                   {/* <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path> */}
                 </g>
               </svg>
-              <input type="text" defaultValue={name} onChange={search} placeholder="Search by title" required />
+              <input type="text" onChange={search} placeholder="Search by title" required />
             </label>
             {/* <div className="validator-hint hidden">Enter valid email address</div> */}
           </div>
