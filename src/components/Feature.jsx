eb6eb4102ee36647b991a8f46d0c010c76bb6359
neya-mul <neyamulislam946@gmail.com>
2026-05-03 16:06:48 +0600

    swiper js added and 2 more json data added

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FeatureCard from './FeatureCard'
 import { features } from '@/lib/data'
+import MySwiper from './Swiper'
 
 export default async function Feature() {
 
@@ -12,11 +13,9 @@ export default async function Feature() {
             <div className='text-center'>
                 <h1 className='text-5xl'>Why choose us ? </h1>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 container mx-auto my-10 p-5' >
-                {
-                    featureBooks.map(feature => <FeatureCard key={feature.id} feature={feature}></FeatureCard>)
-                }
-            </div>
+          
+                <MySwiper featureBooks={featureBooks} />
+           
         </div>
     )
 }
