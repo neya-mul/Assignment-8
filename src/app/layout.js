965229ee4daa3462054218b04c9750fecca8e4af
neya-mul <neyamulislam946@gmail.com>
2026-05-04 17:33:11 +0600

    text colour changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,11 +28,12 @@ export default function RootLayout({ children }) {
   return (
     <html
       style={{ backgroundColor: '#1C1B1F' }}
+      // data-theme="luxury"
       // data-theme="light"
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col text-white-s">
         <ToastContainer />
 
         <Navbar></Navbar>
